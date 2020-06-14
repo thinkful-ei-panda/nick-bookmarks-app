331ca1889153f345083ac74d0c265bb263c29248
nickthorpe71 <nickthorpe71@gmail.com>
2020-06-13 20:37:54 -0500

    fixed all images

--- scripts/templates.js
+++ scripts/templates.js
@@ -45,9 +45,9 @@ function listItemTemplate(id, url, title, rating, description, expanded) {
 
   for (let i = 0; i < 5; i++) {
     if (i < rating) {
-      starArray[i] = '<img src="/images/star-lit.png" alt="Lit Star" />';
+      starArray[i] = '<img src="images/star-lit.png" alt="Lit Star" />';
     } else {
-      starArray[i] = '<img src="/images/star-unlit.png" alt="Unlit Star" />';
+      starArray[i] = '<img src="images/star-unlit.png" alt="Unlit Star" />';
     }
   }
 
@@ -93,7 +93,7 @@ function createStarRatingTemplate() {
       sourceImage = 'unlit';
     }
 
-    startString += `<img id="ratingStar" src="/images/star-${sourceImage}.png" alt="rate ${i}" />`;
+    startString += `<img id="ratingStar" src="images/star-${sourceImage}.png" alt="rate ${i}" />`;
   }
   return startString;
 }
@@ -140,51 +140,51 @@ function footerTemplate() {
     filterMenu = `
       <button class="filter-button force-row active-button" name="filter 5 stars only">
         <img src="images/star-lit.png" alt="Lit Star" /> 
-        <img src="/images/star-lit.png" alt="Lit Star" /> 
-        <img src="/images/star-lit.png" alt="Lit Star" /> 
-        <img src="/images/star-lit.png" alt="Lit Star" /> 
-        <img src="/images/star-lit.png" alt="Lit Star" />
+        <img src="images/star-lit.png" alt="Lit Star" /> 
+        <img src="images/star-lit.png" alt="Lit Star" /> 
+        <img src="images/star-lit.png" alt="Lit Star" /> 
+        <img src="images/star-lit.png" alt="Lit Star" />
         <p>only</p> 
       </button>
       <button class="filter-button force-row active-button" name="filter 4 stars and up">
-        <img src="/images/star-unlit.png" alt="Unlit Star" /> 
-        <img src="/images/star-lit.png" alt="Lit Star" /> 
-        <img src="/images/star-lit.png" alt="Lit Star" /> 
-        <img src="/images/star-lit.png" alt="Lit Star" /> 
-        <img src="/images/star-lit.png" alt="Lit Star" /> 
+        <img src="images/star-unlit.png" alt="Unlit Star" /> 
+        <img src="images/star-lit.png" alt="Lit Star" /> 
+        <img src="images/star-lit.png" alt="Lit Star" /> 
+        <img src="images/star-lit.png" alt="Lit Star" /> 
+        <img src="images/star-lit.png" alt="Lit Star" /> 
         <p>& up</p>
       </button>
       <button class="filter-button force-row active-button" name="filter 3 stars and up">
-        <img src="/images/star-unlit.png" alt="Unlit Star" /> 
-        <img src="/images/star-unlit.png" alt="Unlit Star" /> 
-        <img src="/images/star-lit.png" alt="Lit Star" />  
-        <img src="/images/star-lit.png" alt="Lit Star" /> 
-        <img src="/images/star-lit.png" alt="Lit Star" /> 
+        <img src="images/star-unlit.png" alt="Unlit Star" /> 
+        <img src="images/star-unlit.png" alt="Unlit Star" /> 
+        <img src="images/star-lit.png" alt="Lit Star" />  
+        <img src="images/star-lit.png" alt="Lit Star" /> 
+        <img src="images/star-lit.png" alt="Lit Star" /> 
         <p>& up</p>
       </button>
       <button class="filter-button force-row active-button" name="filter 2 stars and up">
-        <img src="/images/star-unlit.png" alt="Unlit Star" /> 
-        <img src="/images/star-unlit.png" alt="Unlit Star" /> 
-        <img src="/images/star-unlit.png" alt="Unlit Star" /> 
-        <img src="/images/star-lit.png" alt="Lit Star" /> 
-        <img src="/images/star-lit.png" alt="Lit Star" /> 
+        <img src="images/star-unlit.png" alt="Unlit Star" /> 
+        <img src="images/star-unlit.png" alt="Unlit Star" /> 
+        <img src="images/star-unlit.png" alt="Unlit Star" /> 
+        <img src="images/star-lit.png" alt="Lit Star" /> 
+        <img src="images/star-lit.png" alt="Lit Star" /> 
         <p>& up</p>
       </button>
       <button class="filter-button force-row active-button" name="filter 1 star and up">
-        <img src="/images/star-unlit.png" alt="Unlit Star" /> 
-        <img src="/images/star-unlit.png" alt="Unlit Star" /> 
-        <img src="/images/star-unlit.png" alt="Unlit Star" /> 
-        <img src="/images/star-unlit.png" alt="Unlit Star" /> 
-        <img src="/images/star-lit.png" alt="Lit Star" /> 
+        <img src="images/star-unlit.png" alt="Unlit Star" /> 
+        <img src="images/star-unlit.png" alt="Unlit Star" /> 
+        <img src="images/star-unlit.png" alt="Unlit Star" /> 
+        <img src="images/star-unlit.png" alt="Unlit Star" /> 
+        <img src="images/star-lit.png" alt="Lit Star" /> 
         <p>& up</p>
       </button>
     `;
   }
 
   return `
-    <button id="add-bookmark-button" class="nav-button active-button"><img src="/images/new-button.png" alt="new-bookmark" /></button>
+    <button id="add-bookmark-button" class="nav-button active-button"><img src="images/new-button.png" alt="new-bookmark" /></button>
     <div class="dropup">
-      <button class="nav-button drop-button active-button"><img src="/images/filter-button.png" alt="filter" /></button>
+      <button class="nav-button drop-button active-button"><img src="images/filter-button.png" alt="filter" /></button>
       <div class="dropup-content">
         ${filterMenu}
       </div>
